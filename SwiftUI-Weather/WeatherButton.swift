874414fc59ba00b76ac010c280a1ff897657382f
Sherwood Glazier <sglazier@icloud.com>
2024-04-30 11:30:21 -0500

    Initial Download

---
+++ SwiftUI-Weather/WeatherButton.swift
@@ -0,0 +1,32 @@
+//
+//  WeatherButton.swift
+//  SwiftUI-Weather
+//
+//  Created by Sean Allen on 10/27/20.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor.gradient)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold))
+            .cornerRadius(10)
+    }
+}
+
+struct WeatherButton_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherButton(title: "Test Title",
+                      textColor: .white,
+                      backgroundColor: .blue)
+    }
+}
